42d88200a1c4600e36a59164bb889e71cff5d925
sakuma-gif <sakuma@kirinz.tokyo>
2026-04-20 15:49:53 +0900

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://vuwveaqwecvstqtmjmzz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1d3ZlYXF3ZWN2c3RxdG1qbXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NTEwOTgsImV4cCI6MjA5MjIyNzA5OH0.G24DwwyOTjMmgCL-P7jbABIJpali7MhY8AiOQb9tb-U'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
